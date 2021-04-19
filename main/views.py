497770f1b41f8ae2d92140432096176d768c3160
yaroslavgodless <dr.frostman@gmail.com>
2021-04-19 17:33:13 +0300

    i forgot to add commits

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from .models import Movie
+
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movie/movie_list.html", {"movie_list": movies})
+        
