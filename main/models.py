2e6fb6966796732c8817fe176dae072cb8c27d3f
yaroslavgodless <dr.frostman@gmail.com>
2021-04-20 17:27:36 +0300

    second one

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField("Категория", max_length=150)
@@ -58,6 +59,10 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
+    
 
     class Meta:
         verbose_name = "Фильм"
--- main/views.py
+++ main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
-class MoviesView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movie/movie_list.html", {"movie_list": movies})
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+class MovieDetailsView(DetailView):
+    model = Movie
+    slug_field = "url"
         
