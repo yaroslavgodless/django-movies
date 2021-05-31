450c87782dd004beb9a8fab18a4a90a7b5229b16
yaroslavgodless <dr.frostman@gmail.com>
2021-05-31 23:36:59 +0300

    new admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,53 @@
 from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Rating)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name", )
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    list_editable = ("draft",)
+    search_fields = ("title", "category__name")
+    inlines = [ReviewInLine]
+    save_on_top = True
+    save_as = True
+    
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name", "url")
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name", "age")
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    list_display = ("title", "movie")
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("movie", "ip", "star")
+
+
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
